52f7a45c1e76bbb255e87faed2192eb0f49d3a80
Caique <carlosneto726@gmail.com>
2023-05-13 18:33:23 -0300

    terminando de implementar e ajustar o view_cars e ajustes

--- js/car.js
+++ js/car.js
@@ -1,4 +1,26 @@
 import cars from '../json/cars.json' assert { type: 'json' };
+import brands from '../json/brands.json' assert { type: 'json'};
+
+
+        
+function formatadorDePreco(price_value){
+    var formatted_price = '';
+    var price = ''+price_value;
+    var price_length = price.length;
+    var start;
+    while(price_length > 0){
+        start = price_length - 3 > 0 ? price_length - 3 : 0;
+        formatted_price = ' ' + price.substring(start, start+3)+formatted_price;
+        price = price.substring(0, start);
+        price_length = price.length;
+    }
+    formatted_price = formatted_price.replace(/^\s+/, '$').replace(' ', '.');
+    return formatted_price;
+}
+
+function formatadorFloat(numero){
+    return numero.replace('.', ',');
+}
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -6,30 +28,80 @@ var car_id = urlParams.get('carid');
 console.log(car_id)
 
 var car_card = document.getElementById("car-card");
-var h1 = document.createElement("h1");
+var card = document.createElement("div");
+card.classList.add("car-card")
+
 var i = car_id-1;
 
-h1.innerHTML =
-    "<div>"+
-    "    <img src='../img/cars/"+(i+1)+".png'></img>"+
-    "    <p align='left' class='ms-3'>" + 
-    "        Nome:"+cars[i].name+"</br>" + 
-    "        Tipo: "+cars[i].class+" / "+cars[i].type+"</br>" +
-    "        Preço: R$"+cars[i].price+"<br/>"+
-    "        Aceleração: "+cars[i].aceleration+"<br/>"+
-    "        Velocidade: "+cars[i].speed+"<br/>"+
-    "        Manuseio: "+cars[i].handling+"<br/>"+
-    "        Aceleração máximo: "+cars[i].tuned_aceleration+"<br/>"+
-    "        Velocidade máximo: "+cars[i].tuned_speed+"<br/>"+
-    "        Manuseio máximo: "+cars[i].tuned_handling+"<br/>"+
-    "    </p>" +
-    "</div>";
+card.innerHTML =
+    "<center>" +
+    "   <div class='card' style='width: 55rem;'>" +
+    "       <div class='card-body'>" +
+    "          <h1 class='card-title' align='left'>Nome: <span class='fw-bold'>"+cars[i].name+"</span></h1>" + 
+    "          <p class='card-text fs-4' align='left'>Classe: <span class='fw-bold'>"+cars[i].class+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Tipo: <span class='fw-bold'>"+cars[i].type+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Marca: <span class='fw-bold'>"+brands[cars[i].brand_id - 1].name+"</span></p>"+
+    "          <p class='card-text fs-4' align='left'>Preço: <span class='fw-bold'>"+formatadorDePreco(cars[i].price)+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Aceleração: <span class='fw-bold'>"+formatadorFloat(cars[i].aceleration)+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Aceleração modificada: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_aceleration)+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Velocidade máxima: <span class='fw-bold'>"+formatadorFloat(cars[i].speed)+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Velocidade máxima modificada: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_speed)+"</span></p>" +
+    "          <p class='card-text fs-4' align='left'>Manuseio: <span class='fw-bold'>"+formatadorFloat(cars[i].handling)+"</p>" +
+    "          <p class='card-text fs-4' align='left'>Manuseio modificado: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_handling)+"</span></p>" +
+    "      </div>" +
+    "      <img class='card-img-bottom rounded' src='../img/cars/"+(i+1)+".WEBP'></img>" +
+    "   </div>" +
+    "</center>";
+
+car_card.appendChild(card);
 
 
-//+cars[car_id-1]['id'];
 
-"    <img src='img/"+(i+1)+".png' class='card-img-top'>" +
-"    <div class='card-body'>" +
+var page_navigation = document.getElementById("page-navigation");
+var pagination = document.createElement("div");
 
 
-car_card.appendChild(h1);
+
+var minus_current = parseInt(cars[i].id);
+var current = parseInt(cars[i].id);
+var plus_current = parseInt(cars[i].id);
+
+
+var next = parseInt(cars[i].id) + 1;
+var previous = parseInt(cars[i].id) - 1;
+
+if(minus_current <= 3){
+    minus_current = 4
+}else if(plus_current >= 90){
+    plus_current = 91
+}
+
+
+
+
+pagination.innerHTML = 
+"<nav class='page-navigation' id='page-navigation' aria-label='mb-5'>"+
+"    <ul class='pagination justify-content-center mt-5'>"+
+"        <li class='page-item'><a class='page-link' id='anterior' href='car_view.html?carid="+ (previous) +"'>Anterior</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(minus_current - 3)+"'>"+(minus_current - 3)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(minus_current - 2)+"'>"+(minus_current - 2)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(minus_current - 1)+"'>"+(minus_current - 1)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' style='background-color: #161821; color: white;' href='#'>"+current+"</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(plus_current + 1)+"'>"+(plus_current + 1)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(plus_current + 2)+"'>"+(plus_current + 2)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' href='car_view.html?carid="+(plus_current + 3)+"'>"+(plus_current + 3)+"</a></li>"+
+"        <li class='page-item'><a class='page-link' id='proximo' href='car_view.html?carid="+next+"'>Próximo</a></li>"+
+"    </ul>"+
+"</nav>";
+
+
+page_navigation.appendChild(pagination);
+
+
+if(current == 94){
+    var proximo = document.getElementById("proximo");
+    proximo.href = "#"
+}else if(current == 1){
+    var anterior = document.getElementById("anterior");
+    anterior.href = "#"
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import listCars from '../json/cars.json' assert { type: 'json' };
+import listBrands from '../json/brands.json' assert { type: 'json' };
 
 function formatadorDePreco(price_value){
     var formatted_price = '';
@@ -11,8 +12,7 @@ function formatadorDePreco(price_value){
         price = price.substring(0, start);
         price_length = price.length;
     }
-    formatted_price = formatted_price.replace(/^\s+/, '$');
-    formatted_price = formatted_price.replace(' ', '.');
+    formatted_price = formatted_price.replace(/^\s+/, '$').replace(' ', '.');
     return formatted_price;
 }
 
@@ -32,25 +32,27 @@ function showCars(cars){
         element.classList.add('childElement');
 
         element.innerHTML = 
-                            "<div class='car-card card' style='width: 24rem;'>" +
-                            "    <img src='img/cars/"+(cars[i].id)+".png' class='card-img-top'>" +
-                            "    <div class='card-body'>" +
-                            "        <h5 class='name card-title fw-bold' align='left'>"+cars[i].name+"</h5>" + 
-                            "        <p class='type-class card-text' align='left'>"+cars[i].class+" / "+cars[i].type+"</p>" +
-                            "    </div>" +
-                            "    <p align='left' class='ms-3'>" + 
-                            "        <span class='price'>Preço: "+formatadorDePreco(cars[i].price)+"</span><br/>"+
-                            "        <span class='aceleration'>Aceleração: "+formatadorFloat(cars[i].aceleration)+"</span><br/>"+
-                            "        <span class='velocity'>Velocidade: "+formatadorFloat(cars[i].speed)+"</span><br/>"+
-                            "        <span class='handling'>Manuseio: "+formatadorFloat(cars[i].handling)+"</span><br/>"+
-                            "        <span class='tuned-aceleration'>Aceleração máximo: "+formatadorFloat(cars[i].tuned_aceleration)+"</span><br/>"+
-                            "        <span class='tuned-velocity'>Velocidade máximo: "+formatadorFloat(cars[i].tuned_speed)+"</span><br/>"+
-                            "        <span class='tuned-handling'>Manuseio máximo: "+formatadorFloat(cars[i].tuned_handling)+"</span><br/>"+
-                            "    </p>" +
-                            "    <div class='card-body' align='left'>" +
-                            "        <a href='sections/car_view.html?carid="+cars[i].id+"' class='btn btn-primary'>Mais Informações</a>" +
-                            "    </div>" +
-                            "</div>" ;
+        "<div class='car-card card' style='width: 24rem;'>" +
+        "    <img src='img/cars/"+(cars[i].id)+".WEBP' class='card-img-top rounded-1'>" +
+        "    <div class='card-body'>" +
+        "        <h4 class='name border-bottom border-secondary card-title fw-bold' align='left'>"+cars[i].name+"</h4>" + 
+        "        <span class='type-class fw-bold fs-5'><p class='d-inline-flex border-bottom border-secondary card-text' align='left'>"+cars[i].class+" / "+cars[i].type+"</p></span>" +
+        "    </div>" +
+        "    <p align='left' class='ms-3'>" + 
+        "        <span class='border-bottom border-secondary brand'>Marca: <span class='fw-bold'>"+listBrands[cars[i].brand_id - 1].name+"</span></span><br/>"+
+        "        <span class='border-bottom border-secondary price'>Preço: <span class='fw-bold'>"+formatadorDePreco(cars[i].price)+"</span></span><br/>"+
+        "        <span class='aceleration'>Aceleração: <span class='fw-bold'>"+formatadorFloat(cars[i].aceleration)+"</span></span><br/>"+
+        "        <span class='border-bottom border-secondary tuned-aceleration'>Aceleração modificada: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_aceleration)+"</span></span><br/>"+
+        "        <span class='velocity'>Velocidade máxima: <span class='fw-bold'>"+formatadorFloat(cars[i].speed)+"</span></span><br/>"+
+        "        <span class='border-bottom border-secondary tuned-velocity'>Velocidade máxima modificada: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_speed)+"</span></span><br/>"+
+        "        <span class='handling'>Manuseio: <span class='fw-bold'>"+formatadorFloat(cars[i].handling)+"</span></span><br/>"+
+        "        <span class='border-bottom border-secondary tuned-handling'>Manuseio modificado: <span class='fw-bold'>"+formatadorFloat(cars[i].tuned_handling)+"</span></span><br/>"+
+        
+        "    </p>" +
+        "    <div class='card-body' align='left'>" +
+        "        <a href='sections/car_view.html?carid="+cars[i].id+"' class='btn btn-primary'>Ver melhor</a>" +
+        "    </div>" +
+        "</div>" ;
                             
         container.appendChild(element);
 
@@ -77,43 +79,21 @@ export function searchCar(){
 
 export function orderby(btn){
     var acao = btn.name
+    var ordernar_btn = document.getElementById("ordernar-btn");
 
-    if(acao == "padrao"){
-        showCars(listCars.sort(function(a, b){return a.id - b.id}));
-    }
-    else if(acao == "alfa"){
-        showCars(listCars.sort(function(a, b){return a.name.localeCompare(b.name)}));
-    }
-    else if(acao == "preco"){
-        showCars(listCars.sort(function(a, b){return b.price - a.price}));
-    }
-    else if(acao == "classe"){
-        showCars(listCars.sort(function(a, b){return a.class.localeCompare(b.class)}));
-    }
-    else if(acao == "tipo"){
-        showCars(listCars.sort(function(a, b){return a.type.localeCompare(b.type)}));
-    }
-    else if(acao == "ace_p"){
-        showCars(listCars.sort(function(a, b){return b.aceleration - a.aceleration}));
-    }
-    else if(acao == "ace_m"){
-        showCars(listCars.sort(function(a, b){return b.tuned_aceleration - a.tuned_aceleration}));
-    }
-    else if(acao == "vel_p"){
-        showCars(listCars.sort(function(a, b){return b.speed - a.speed}));
-    }
-    else if(acao == "vel_m"){
-        showCars(listCars.sort(function(a, b){return b.tuned_speed - a.tuned_speed}));
-    }
-    else if(acao == "man_p"){
-        showCars(listCars.sort(function(a, b){return b.handling - a.handling}));
-    }
-    else if(acao == "man_m"){
-        showCars(listCars.sort(function(a, b){return b.tuned_handling - a.tuned_handling}));
-    }
-    else if(acao == "remix"){
-        showCars(listCars.sort(function(a, b){return b.remix - a.remix}));
-    }
+    if(acao == "padrao"){showCars(listCars.sort(function(a, b){return a.id - b.id})); ordernar_btn.innerHTML = "Ordernar por 'Padrão'"}
+    else if(acao == "marca"){showCars(listCars.sort(function(a, b){return a.brand_id.localeCompare(b.brand_id)})); ordernar_btn.innerHTML = "Ordernar por 'Marca'"}
+    else if(acao == "alfa"){showCars(listCars.sort(function(a, b){return a.name.localeCompare(b.name)})); ordernar_btn.innerHTML = "Ordernar por 'Alfabeto'"}
+    else if(acao == "preco"){showCars(listCars.sort(function(a, b){return b.price - a.price})); ordernar_btn.innerHTML = "Ordernar por 'Preço'"}
+    else if(acao == "classe"){showCars(listCars.sort(function(a, b){return a.class.localeCompare(b.class)})); ordernar_btn.innerHTML = "Ordernar por 'Classe'"}
+    else if(acao == "tipo"){showCars(listCars.sort(function(a, b){return a.type.localeCompare(b.type)})); ordernar_btn.innerHTML = "Ordernar por 'Tipo'"}
+    else if(acao == "ace_p"){showCars(listCars.sort(function(a, b){return b.aceleration - a.aceleration})); ordernar_btn.innerHTML = "Ordernar por 'Aceleração'"}
+    else if(acao == "ace_m"){showCars(listCars.sort(function(a, b){return b.tuned_aceleration - a.tuned_aceleration})); ordernar_btn.innerHTML = "Ordernar por 'Aceleração modificada'"}
+    else if(acao == "vel_p"){showCars(listCars.sort(function(a, b){return b.speed - a.speed})); ordernar_btn.innerHTML = "Ordernar por 'Velocidade'"}
+    else if(acao == "vel_m"){showCars(listCars.sort(function(a, b){return b.tuned_speed - a.tuned_speed})); ordernar_btn.innerHTML = "Ordernar por 'Velocidade modificada'"}
+    else if(acao == "man_p"){showCars(listCars.sort(function(a, b){return b.handling - a.handling})); ordernar_btn.innerHTML = "Ordernar por 'Manuseio'"}
+    else if(acao == "man_m"){showCars(listCars.sort(function(a, b){return b.tuned_handling - a.tuned_handling})); ordernar_btn.innerHTML = "Ordernar por 'Manuseio modificado'"}
+    else if(acao == "remix"){showCars(listCars.sort(function(a, b){return b.remix - a.remix})); ordernar_btn.innerHTML = "Ordernar por 'Remix'"}
     
 }
 
@@ -122,6 +102,7 @@ export function orderby(btn){
 export function switchAttrs(btn){
     var cards = document.getElementsByClassName("car-card");
     var preco = document.getElementsByClassName("price");
+    var brand = document.getElementsByClassName("brand");
     var aceleracao = document.getElementsByClassName("aceleration");
     var velocidade = document.getElementsByClassName("velocity");
     var manuseio = document.getElementsByClassName("handling");
@@ -132,21 +113,43 @@ export function switchAttrs(btn){
     var element = btn.name;
 
     for(let i = 0; i < cards.length; i++){
-        if(element === "preco"){preco[i].hidden = !(preco[i].hidden)}
-        if(element === "classe-tipo"){tipo_class[i].hidden = !(tipo_class[i].hidden)}
-        //if(element === "marca"){[i].hidden = !([i].hidden)}
-        if(element === "aceleracao-padrao"){aceleracao[i].hidden = !(aceleracao[i].hidden)}
-        if(element === "aceleracao-modificada"){aceleracao_mod[i].hidden = !(aceleracao_mod[i].hidden)}
-        if(element === "velocidade-padrao"){velocidade[i].hidden = !(velocidade[i].hidden)}
-        if(element === "velocidade-modificada"){velocidade_mod[i].hidden = !(velocidade_mod[i].hidden)}
-        if(element === "manuseio-padrao"){manuseio[i].hidden = !(manuseio[i].hidden)}
-        if(element === "manuseio-modificado"){manuseio_mod[i].hidden = !(manuseio_mod[i].hidden)}
-        
+        if(element === "preco"){preco[i].hidden = !(preco[i].hidden); window.sessionStorage.setItem("preco", preco[i].hidden);}
+        if(element === "classe-tipo"){tipo_class[i].hidden = !(tipo_class[i].hidden); window.sessionStorage.setItem("tipo_class", tipo_class[i].hidden);}
+        if(element === "marca"){brand[i].hidden = !(brand[i].hidden); window.sessionStorage.setItem("brand", brand[i].hidden);}
+        if(element === "aceleracao-padrao"){aceleracao[i].hidden = !(aceleracao[i].hidden); window.sessionStorage.setItem("aceleracao", aceleracao[i].hidden);}
+        if(element === "aceleracao-modificada"){aceleracao_mod[i].hidden = !(aceleracao_mod[i].hidden); window.sessionStorage.setItem("aceleracao_mod", aceleracao_mod[i].hidden);}
+        if(element === "velocidade-padrao"){velocidade[i].hidden = !(velocidade[i].hidden); window.sessionStorage.setItem("velocidade", velocidade[i].hidden);}
+        if(element === "velocidade-modificada"){velocidade_mod[i].hidden = !(velocidade_mod[i].hidden); window.sessionStorage.setItem("velocidade_mod", velocidade_mod[i].hidden);}
+        if(element === "manuseio-padrao"){manuseio[i].hidden = !(manuseio[i].hidden); window.sessionStorage.setItem("manuseio", manuseio[i].hidden);}
+        if(element === "manuseio-modificado"){manuseio_mod[i].hidden = !(manuseio_mod[i].hidden); window.sessionStorage.setItem("manuseio_mod", manuseio_mod[i].hidden);}
     }            
+}
+
+function booleanConverter(str){
+    var myString = str
+    if(myString === 'true'){
+        return 1;
+    }else if(myString === 'false'){
+        return 0;
+    }
 }
 
 
 
+
+var input_check = document.getElementsByClassName("form-check-input");
+for(let i = 0; i < input_check.length; i++){
+    if(input_check[i].name == "preco"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("preco"))}
+    if(input_check[i].name == "classe-tipo"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("tipo_class"))}
+    if(input_check[i].name == "marca"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("brand"))}
+    if(input_check[i].name == "aceleracao-padrao"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("aceleracao"))}
+    if(input_check[i].name == "aceleracao-modificada"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("aceleracao_mod"))}
+    if(input_check[i].name == "velocidade-padrao"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("velocidade"))}
+    if(input_check[i].name == "velocidade-modificada"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("velocidade_mod"))}
+    if(input_check[i].name == "manuseio-padrao"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("manuseio"))}
+    if(input_check[i].name == "manuseio-modificado"){input_check[i].checked = booleanConverter(window.sessionStorage.getItem("manuseio_mod"))}
+
+}
 
 
 showCars(listCars);
